e0bce74de3409dacd9ec758204a8652fb6c80218
Raed-93 <tbilhraed@gmail.com>
2023-12-07 13:14:58 +0200

    pages view not finsh yet

--- weCare/weCare_App/urls.py
+++ weCare/weCare_App/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.root)
+    path('',views.root),
+    path('hospital',views.hospital)
 ]
--- weCare/weCare_App/views.py
+++ weCare/weCare_App/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def root(request):
-    pass
+
+    return render(request,"dr_page.html")
+
+def hospital(request):
+ return render(request,"hospital.html")
