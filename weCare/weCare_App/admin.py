5891b78318b9f71d36e123345e13b41561592baf
habibGrande <snomo22dy@gmail.com>
2023-12-08 13:29:19 +0200

    admin page and it users

--- weCare/weCare_App/admin.py
+++ weCare/weCare_App/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
+# import django.apps
 
 # Register your models here.
+
+
+admin.site.register(Patient)
+admin.site.register(Feedback)
+admin.site.register(Speciality)
+admin.site.register(Appointment)
+admin.site.register(Location)
+admin.site.register(Hospital)
+
+# models = django.apps.apps.get_models()
+
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
+        
